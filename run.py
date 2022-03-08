da92f1fc18c596204c18072856aa63ffb0c5d4f0
tin2tin <tintwotin@gmail.com>
2022-03-08 16:41:43 +0100

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-
-dir = r"/home/magag/text_to_speech/__init__.py"
-
-if os.name == 'nt':
-    dir = r"C:\Users\marco\blender-text-to-speech"
-
-exec(compile(open(dir).read(), dir, 'exec'))
